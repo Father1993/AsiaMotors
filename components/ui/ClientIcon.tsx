2e3f3753ff1abbbb6548d85aacf8b150adced0c3
Father1993 <enjoyhillol@gmail.com>
2024-11-12 15:43:54 +1000

    add benefits section

---
+++ components/ui/ClientIcon.tsx
@@ -0,0 +1,19 @@
+const ClientIcon = () => {
+    return (
+        <svg
+            className="w-10 h-10 text-red-600"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+            />
+        </svg>
+    )
+}
+
+export default ClientIcon
