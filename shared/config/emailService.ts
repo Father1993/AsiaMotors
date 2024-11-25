2fc01e4e6f8709c1e17fe1b6258c572c9e95d0e9
Father1993 <enjoyhillol@gmail.com>
2024-11-25 13:08:38 +1000

    add new template for spares form with emailjs send mail

--- shared/config/emailService.ts
+++ shared/config/emailService.ts
@@ -11,9 +11,19 @@ if (!process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID) {
 if (!process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY) {
     throw new Error('NEXT_PUBLIC_EMAILJS_PUBLIC_KEY is not defined')
 }
+if (!process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID_2) {
+    throw new Error('NEXT_PUBLIC_EMAILJS_PUBLIC_KEY_2 is not defined')
+}
 
 export const emailConfig: EmailConfig = {
     serviceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
     templateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+    publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+}
+
+// Добавляем новый конфиг для формы запчастей
+export const sparesEmailConfig: EmailConfig = {
+    serviceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    templateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID_2,
     publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
 }
