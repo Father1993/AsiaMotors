6388dce1c62a2b252f17b3b68d44a22ea597fe04
Father1993 <enjoyhillol@gmail.com>
2024-12-02 16:43:27 +1000

    fix bugs

--- components/templates/CatalogPage/CarPage/CarPage.cleint.tsx
+++ components/templates/CatalogPage/CarPage/CarPage.cleint.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { motion, AnimatePresence } from 'framer-motion'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
 import {
     FiArrowLeft,
     FiHeart,
