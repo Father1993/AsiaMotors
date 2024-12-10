355305ca6ba6911e3b19a9e14245baf48d195837
Father1993 <enjoyhillol@gmail.com>
2024-12-10 15:24:53 +1000

    add new page NewCars, add modal popup in page, change color in scroll-bar, fix orderCarModal- add privacy link in text

---
+++ shared/constants/newCarsPage.ts
@@ -0,0 +1,36 @@
+import { GiFactory } from 'react-icons/gi'
+import { TbCheckupList } from 'react-icons/tb'
+import { BiSupport } from 'react-icons/bi'
+
+export const newCarsBenefits = [
+    {
+        title: 'Прямые поставки',
+        description: 'Работаем напрямую с производителями из Китая',
+        icon: GiFactory,
+    },
+    {
+        title: 'Гарантия качества',
+        description: 'Предпродажная проверка каждого автомобиля',
+        icon: TbCheckupList,
+    },
+    {
+        title: 'Полное сопровождение',
+        description: 'От выбора до постановки на учёт',
+        icon: BiSupport,
+    },
+]
+
+export const newPopularBrands = [
+    { name: 'BYD', logo: '/img/brands/BYD-Logo.png' },
+    { name: 'Chery', logo: '/img/brands/Chery-Logo.png' },
+    { name: 'Geely', logo: '/img/brands/Geely-Logo.jpg' },
+    { name: 'Haval', logo: '/img/brands/Haval-Logo.png' },
+    { name: 'GAC', logo: '/img/brands/GAC-Group-Logo.png' },
+    { name: 'Changan', logo: '/img/brands/Changan-Logo.png' },
+    { name: 'Great-Wall', logo: '/img/brands/Great-Wall-logo.png' },
+    { name: 'Li-Auto', logo: '/img/brands/Li-Auto-Logo.png' },
+    { name: 'Zeekr', logo: '/img/brands/Zeekr-logo.png' },
+    { name: 'Avatr', logo: '/img/brands/Avatr-Logo.png' },
+    { name: 'Xiaomi', logo: '/img/brands/Xiaomi-logo.png' },
+    { name: 'Dongfeng', logo: '/img/brands/Dongfeng-logo.png' },
+]
